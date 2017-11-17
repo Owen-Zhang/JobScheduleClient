f8ccb48e47b37465ccc391222fd3c380f7995244
Owen <1034992601@qq.com>
2017-11-17 17:58:32 +0800

    change

--- src/client/main.go
+++ src/client/main.go
@@ -4,7 +4,8 @@ import "github.com/gin-gonic/gin"
 
 func main() {
 	route := gin.Default()
-	route.GET("/ping", func(c *gin.Context) {
+	user := route.Group("/user")
+	user.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "ok"})
 	})
 	route.Run("127.0.0.1:8899")
