622fe4916b5712a7ed716a87593816b553f56df2
Owen <1034992601@qq.com>
2017-11-17 14:49:20 +0800

    add gin

--- src/client/main.go
+++ src/client/main.go
@@ -1,10 +1,11 @@
 package main
 
-import "fmt"
+import "github.com/gin-gonic/gin"
 
 func main() {
-	fmt.Println("11122222")
-	for {
-		fmt.Println("dddddddd")
-	}
+	route := gin.Default()
+	route.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{"message": "ok"})
+	})
+	route.Run("127.0.0.1:8899")
 }
