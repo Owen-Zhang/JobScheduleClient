ea6b1ffd0935e37f740c418be05012cbb5476608
Owen-Zhang <1034992601@qq.com>
2017-12-08 22:32:33 +0800

    fix bug

--- src/FileLoad/main.go
+++ src/FileLoad/main.go
@@ -1,16 +1,23 @@
 package main
 
 
-import "os"
-import "io"
-import "fmt"
 import (
-	"io/ioutil"
 	"net/http"
+	"encoding/json"
 	"encoding/base64"
+	"io/ioutil"
+	"os"
+	"fmt"
 )
 
+type Fileinfo struct {
+	FilePath 		string `json:"filepath"`       //文件路径，如：order/detail
+	FileSuffixName  string `json:"filesuffixname"` //文件的后缀名 如: exe, jpg
+	FileContent  	string `json:"filecontent"`    //文件的byte的64编码
+}
+
 func main() {
+	/*
 	fileopen, err1 := os.Open("./TEST.rar")
 	if (err1 != nil) {
 		fmt.Println(err1.Error())
@@ -38,7 +45,7 @@ func main() {
 	file.Close()
 	
 	
-	
+	*/
 	http.HandleFunc("/upload", uploadHandler)
 	http.Handle("/", http.StripPrefix("/",http.FileServer(http.Dir("./staticfile"))))
 	fmt.Println("文件下载")
@@ -47,39 +54,38 @@ func main() {
 }
 
 func uploadHandler (w http.ResponseWriter, r *http.Request) {
-	bytes := new ([]byte)
-	r.Body.Read(*bytes)
-
 	switch r.Method {
 		case "POST":
-			err := r.ParseMultipartForm(100000)
-	        if err != nil {
-	            http.Error(w, err.Error(), http.StatusInternalServerError)
-	            return
-	        }
-			m := r.MultipartForm
-			files := m.File["uploadfile"]
-
-	        for i, _ := range files {
-	            //for each fileheader, get a handle to the actual file
-	            file, err := files[i].Open()
-	            defer file.Close()
-	            if err != nil {
-	                http.Error(w, err.Error(), http.StatusInternalServerError)
-	                return
-	            }
-				dst, err := os.Create("./staticfile/" + files[i].Filename)
-	            defer dst.Close()
-	            if err != nil {
-	                http.Error(w, err.Error(), http.StatusInternalServerError)
-	                return
-	            }
-	            //copy the uploaded file to the destination file
-	            if _, err := io.Copy(dst, file); err != nil {
-	                http.Error(w, err.Error(), http.StatusInternalServerError)
-	                return
-	            }
+			by, err1 := ioutil.ReadAll(r.Body)
+			if err1 != nil {
+				fmt.Println(err1)
+				return
 			}
+			//b := new(bytes.Buffer)
+			//b.Write(by)
+
+			body := &Fileinfo{}
+			err := json.Unmarshal(by, body)
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+
+			//保存文件 此处还要创建文件夹
+			file, err := os.Create(fmt.Sprintf("%s/%s/%s.%s", "./staticfile", body.FilePath, "XXX", body.FileSuffixName))
+			if err != nil {
+				fmt.Println(err.Error())
+				return
+			}
+			filecontent, err3 := base64.StdEncoding.DecodeString(body.FileContent)
+			if err3 != nil {
+				fmt.Println(err3.Error())
+				return
+			}
+
+			file.Write(filecontent)
+			file.Close()
+
 		default:
         	w.WriteHeader(http.StatusMethodNotAllowed)
 	}
